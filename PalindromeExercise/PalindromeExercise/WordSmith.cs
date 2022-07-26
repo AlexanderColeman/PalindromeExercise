16d77edfcadc189d85afc08fd501b1a29af80650
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-26 07:09:10 -0500

    first commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            string newString = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                newString += str[i];
+            }
+
+            return newString.ToLower() == str.ToLower();
+        } 
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,19 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("daycare",false)]
+        [InlineData("Abba", true)]
+        [InlineData("Detartrated", true)]
+        [InlineData("building", false)]
+        public void Test1(string str, bool expected)
         {
+            var word = new WordSmith();
+
+            var actual = word.IsAPalindrome(str);
+
+            Assert.Equal(expected, actual);
 
         }
     }
